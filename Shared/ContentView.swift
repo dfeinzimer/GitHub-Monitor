ff640745314fe895177b599bf045399115045c0b
David Feinzimer <dfeinzimer@gmail.com>
2022-06-02 20:04:32 -0700

    Update ContentView.swift

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var gitHubTestMessage: String = "Hello, world!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(gitHubTestMessage)
+            .frame(width: 300, height: 300)
+            .onAppear {
+                Task {
+                    let message = try? await testGitHubConnection()
+                    gitHubTestMessage = message ?? "Error"
+                }
+            }
     }
 }
 
@@ -18,4 +26,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func testGitHubConnection() async throws -> String {
+    let url = URL(string: "https://api.github.com/zen")!
+    let urlRequest = URLRequest(url: url)
+    let (data, _) = try await URLSession.shared.data(for: urlRequest)
+    return String(data: data, encoding: .utf8) ?? ""
 }
