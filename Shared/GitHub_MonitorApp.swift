95b155b9c9ca43b139456844706ac7676c787895
David Feinzimer <dfeinzimer@gmail.com>
2022-06-02 19:31:18 -0700

    Initial Commit

---
+++ Shared/GitHub_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_MonitorApp.swift
+//  Shared
+//
+//  Created by David on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
